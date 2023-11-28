ed4bce782eafdec0fe92d7868c8389b960f6a332
veektoh <victorayomitide@gmail.com>
2023-11-28 13:52:10 +0100

    Updates

---
+++ precache-manifest.49be65f3d29d159313e41032f889e9b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a428cc99cf5a28533c9198dd66d83c",
+    "url": "/Veektoh/tictactoe-game/index.html"
+  },
+  {
+    "revision": "31a48ed1b1d162194061",
+    "url": "/Veektoh/tictactoe-game/static/css/main.d81823a6.chunk.css"
+  },
+  {
+    "revision": "ae6468c9d67cd1eeaa74",
+    "url": "/Veektoh/tictactoe-game/static/js/2.d30c3f3c.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Veektoh/tictactoe-game/static/js/2.d30c3f3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2562d4959f8ba4fe8057",
+    "url": "/Veektoh/tictactoe-game/static/js/3.17057f1f.chunk.js"
+  },
+  {
+    "revision": "31a48ed1b1d162194061",
+    "url": "/Veektoh/tictactoe-game/static/js/main.9646a212.chunk.js"
+  },
+  {
+    "revision": "db732f7039b7b85a0ae6",
+    "url": "/Veektoh/tictactoe-game/static/js/runtime-main.a7873843.js"
+  }
+]);
